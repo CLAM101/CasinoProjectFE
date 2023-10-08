a39e60caf504965fd88cad2f7b7febfc1d3c3c6e
root <root@Darren>
2023-10-08 21:03:47 +0200

    testnet contracts

--- src/app/services/ethersService/contracts.ts
+++ src/app/services/ethersService/contracts.ts
@@ -1,2 +1,2 @@
-export const casino = '0xdc64a140aa3e981100a9beca4e685f962f0cf6c9';
-export const token = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+export const casino = '0x71923A568dfBB672ba575b59A9153237E507d27a';
+export const token = '0x59b3de2bE163c88e8c3764bBc9A69E85095BA062';
